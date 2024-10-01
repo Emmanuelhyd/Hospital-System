51eb10df15aa92b9a946a819edba9d52a506d0aa
Bhargavi <bhargavi@iitsofttech.com>
2024-10-01 19:56:34 +0530

    added 1/10/24 Bhargavi

---
+++ Hospital System/Models/MComplaint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hospital_System.Models
+{
+    public class MComplaint
+    {
+       
+            [Required(ErrorMessage = "This field is required.")]
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "Name is required.")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Please fill your complaints.")]
+            public string Complaint { get; set; }
+
+            [Required(ErrorMessage = "This field is required.")]
+            [Phone(ErrorMessage = "Invalid phone number.")]
+            public string PhoneNumber { get; set; }
+
+            [Required(ErrorMessage = "This field is required.")]
+            public string Reply { get; set; }
+        
+
+
+    }
+}
