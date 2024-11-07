39aac483faaa38f858059d706a938d83a9c65f81
Bhargavi <bhargavi@iitsofttech.com>
2024-11-07 14:12:22 +0530

    added commit

---
+++ Hospital System/Models/DriverDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital_System.Models
+{
+    public class DriverDo
+    {
+        public int Id { get; set; }
+
+        public string DriverName { get; set; }
+        public int DriverId { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        public string CNIC { get; set; }
+    }
+}
