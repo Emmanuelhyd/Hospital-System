f52ca90efee71b360c5ea64e8d93574e96f7b920
AnandaSagar-18 <anandasagar@iitsofttech.com>
2024-11-21 15:34:37 +0530

    aa-aa

--- Hospital System/Controllers/ReceptionAdminController.cs
+++ Hospital System/Controllers/ReceptionAdminController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using Hospital_System.Models;
+using Hospital_System.BAL;
+using Hospital_System.DAL;
+using System.Reflection;
+using Hospital_System.Viewmodel;
 
 namespace Hospital_System.Controllers
 {
     public class ReceptionAdminController : Controller
     {
+        PatientBAL patientBAL = new PatientBAL();
         // GET: ReceptionAdmin
         public ActionResult ReceptionFrontPage()
         {
-            return View();
+            var doctor = patientBAL.GetDoctors();
+            var model = new Allview
+            {
+
+                Doctors = doctor
+            };
+            return View(model);
         }
+
+       
     }
 }
